536587659c386e9181f16eccfe2961c7c7f0d6ca
AshwiniNakod <nakodashwini@gmail.com>
2022-11-05 18:30:34 +0530

    api changed

--- src/global.js
+++ src/global.js
@@ -1,5 +1,5 @@
 // export const API = `https://6341893e16ffb7e275d36add.mockapi.io`
-// export const API = `https://ashwini-node-app.herokuapp.com`
-export const API = `http://localhost:4000`
+export const API = `https://ashwini-node-app.herokuapp.com`
+// export const API = `http://localhost:4000`
 
 
